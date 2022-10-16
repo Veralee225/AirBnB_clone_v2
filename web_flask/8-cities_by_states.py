642cfd992c66b841beb67cbda20ec013237724e1
Veralee225 <Veralee225@gmail.com>
2022-10-16 16:11:25 +0100

    feat: tasks

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,6 +4,7 @@ starts a Flask web application
 """
 
 from flask import Flask, render_template
+from models import *
 
 app = Flask(__name__)
 
@@ -45,6 +46,23 @@ def number_template(n):
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
     return render_template('6-number_odd_or_even.html', num=n)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    return render_template('7-states_list.html',
+                           states=storage.all("State"))
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    return render_template('8-cities_by_states.html',
+                           states=storage.all("State"))
+
+
+@app.teardown_appcontext
+def teardown(err):
+    storage.close()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
